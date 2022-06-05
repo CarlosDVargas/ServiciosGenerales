58ba78ac0ecf38678179fa7f86451990299f61f7
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.com>
2022-06-05 13:02:49 -0600

    Se agregan las vistas y controladores del modelo User_Account

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :user_accounts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "pages#home"
+
+  devise_for :user_accounts, controllers: {
+                             sessions: "user_accounts/sessions",
+                             registrations: "user_accounts/registrations",
+                           }
 end
