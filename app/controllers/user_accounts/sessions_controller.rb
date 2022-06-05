17ddf986964848250fe0363402feb320972f3957
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.com>
2022-06-05 13:18:28 -0600

    Se agrega el módulo de inicio de sesión y edición de la cuenta del usuario

--- app/controllers/user_accounts/sessions_controller.rb
+++ app/controllers/user_accounts/sessions_controller.rb
@@ -4,19 +4,19 @@ class UserAccounts::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  #def new
+  #  super
+  #end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  #POST /resource/sign_in
+  def create
+    super
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  #DELETE /resource/sign_out
+  def destroy
+    super
+  end
 
   # protected
 
