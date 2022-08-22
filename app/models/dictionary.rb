d1a888292edc05c5ad3825fd0b648fdf075872c1
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.info>
2022-08-21 20:26:34 -0600

    Se realizan cambios generales en los controladores cumpliendo los estándares utilizados para Rails.

--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 class Dictionary
   attr_reader :requesterTypes, :workBuildings, :workTypes, :studentAssociations, :requestStatuses
 
   def initialize
-    @requesterTypes = { teacher: "Docente", administrator: "Administativo", student: "Estudiante" }
-    @studentAssociations = { assosiation: "Asociación Estudiantil", feucr: "FEUCR" }
-    @workBuildings = { gym: "Gimnasio", sciencie_labs: "Laboratiorios Ciencias Generales", cafeteria: "Comedor", other: "Otro" }
-    @workTypes = { electrical: "Eléctrico", plumbing: "Plomería", wood: "Ebanistería", other: "Otro" }
-    @requestStatuses = { in_process: "En proceso", completed: "Completado", closed: "Cerrado", denied: "Denegado" }
+    @requesterTypes = { teacher: 'Docente', administrator: 'Administativo', student: 'Estudiante' }
+    @studentAssociations = { assosiation: 'Asociación Estudiantil', feucr: 'FEUCR' }
+    @workBuildings = { gym: 'Gimnasio', sciencie_labs: 'Laboratiorios Ciencias Generales', cafeteria: 'Comedor',
+                       other: 'Otro' }
+    @workTypes = { electrical: 'Eléctrico', plumbing: 'Plomería', wood: 'Ebanistería', other: 'Otro' }
+    @requestStatuses = { in_process: 'En proceso', completed: 'Completado', closed: 'Cerrado', denied: 'Denegado' }
   end
 end
