03f879334640c1a3c826f11dae271b4895b8c37c
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.info>
2022-10-04 17:03:06 -0600

    Se hacen correcciones solicitadas respecto el formulario de la solicitudes

--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -6,9 +6,15 @@ class Dictionary
   def initialize
     @requesterTypes = { teacher: 'Docente', administrator: 'Administativo', student: 'Estudiante' }
     @studentAssociations = { association: 'Asociación Estudiantil', feucr: 'FEUCR' }
-    @workBuildings = { gym: 'Gimnasio', sciencie_labs: 'Laboratiorios Ciencias Generales', cafeteria: 'Comedor',
-                       other: 'Otro' }
-    @workTypes = { electrical: 'Eléctrico', plumbing: 'Plomería', wood: 'Ebanistería', other: 'Otro' }
+    @workBuildings = { social_action: 'Edificio Acción Social', auditorium: 'Edificio Auditorio', investigation: 'Edificio Investigación',
+                       sciencie_labs: 'Edificio Ciencias Naturales', direction: 'Edificio Dirección', vie: 'Edificio Vida Estudiantil',
+                       library: 'Edificio Biblioteca', teacher: 'Edificio Módulo Docente', humanities: 'Edificio Humanidades',
+                       engineering: 'Edificio Ingeniería', music: 'Edificio Conservatorio de Música', health: 'Edificio Salud',
+                       classrooms_100: 'Conjunto de aulas 100', classrooms_200: 'Conjunto de aulas 200', classrooms_300: 'Conjunto de aulas 300',
+                       museum: 'Museo', residences: 'Residencias estudiantiles', kiosk: 'Kiosco', cafeteria: 'Comedor Estudiantil',
+                       CILEM: 'CILEM', other: 'Otro' }
+    @workTypes = { electrical: 'Eléctrico', plumbing: 'Fontanería', locksmith: 'Cerrajería', glassware: 'Vidriería',
+                   tinsmith: 'Hojalatería', painting: 'Pintura', other: 'Otro' }
     @requestStatuses = { in_process: 'En proceso', completed: 'Completado', closed: 'Cerrado', denied: 'Denegado' }
   end
 end
