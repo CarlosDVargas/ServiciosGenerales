cfee9ca0636eca8deaaef3eec632557bf633fa67
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.info>
2023-01-11 21:00:55 -0600

    Se edita el envío de correos para usarse en Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 465,
+    authentication: :login,
+    user_name: ENV["MAIL_USERNAME"],
+    password: ENV["MAIL_PASSWORD"],
+    enable_starttls_auto: true,
+    openssl_verify_mode: 'none',
+    ssl: true,
+    tls: true
+  }
 end
