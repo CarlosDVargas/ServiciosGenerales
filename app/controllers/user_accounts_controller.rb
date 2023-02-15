8d7701783ea37cd23c824892400094506b673515
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.info>
2023-02-14 21:13:04 -0600

    Se trabaja sobre los filtros de Ransack para los usuarios

--- app/controllers/user_accounts_controller.rb
+++ app/controllers/user_accounts_controller.rb
@@ -1,18 +1,11 @@
 class UserAccountsController < ApplicationController
-  before_action :set_role, only: %i[new index]
+  before_action :set_role, only: %i[new index search]
+  before_action :set_user_account, only: %i[show edit update destroy]
 
   def index
-    byebug
     @employees = UserAccount.where(campus: current_user_account.campus)
-    if params[:q].present?
-      @type ||= params[:q][:role_eq]
-    end
-    @employees_selected = if @role == 'worker'
-                            @employees.where(role: 'worker')
-                          else
-                            @employees.where(role: 'admin')
-                          end
-    @query = @employees_selected.ransack(params[:q])
+    employees_selected = @employees.where(role: @role)
+    @query = employees_selected.ransack(params[:q])
     @employees = @query.result
   end
 
@@ -27,7 +20,17 @@ class UserAccountsController < ApplicationController
   private
 
   def set_role
-    @role = params[:role]
+    @role = params[:role] if params[:role]
+    if params[:q] && params[:q][:role_eq]
+      if params[:q][:role_eq] == 'worker' || params[:q][:role_eq] == '1'
+        @role = 1
+      else
+        @role = 0
+      end
+    end
   end
 
+  def set_user_account
+    @user_account = UserAccount.find(params[:id])
+  end
 end
