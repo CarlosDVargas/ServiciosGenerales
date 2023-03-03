a1209783d080357dfb68eda593c10fd5569beb56
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.info>
2023-03-03 13:13:44 -0600

    Se corrige error en el cambio de contraseña

--- app/controllers/user_accounts/registrations_controller.rb
+++ app/controllers/user_accounts/registrations_controller.rb
@@ -32,6 +32,7 @@ class UserAccounts::RegistrationsController < Devise::RegistrationsController
 
   # PUT /resource
   def update
+    byebug
     super
   end
 
@@ -57,9 +58,9 @@ class UserAccounts::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
