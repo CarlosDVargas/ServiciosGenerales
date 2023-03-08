e8b047a06efd68b689846b26c5e52fe9332c65aa
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.info>
2023-03-08 12:50:19 -0600

    Se eliminan las sentencias de byebug

--- app/controllers/user_accounts/registrations_controller.rb
+++ app/controllers/user_accounts/registrations_controller.rb
@@ -8,14 +8,14 @@ class UserAccounts::RegistrationsController < Devise::RegistrationsController
 
   # GET /resource/sign_up
   def new
-    #byebug
+    
     @user_account = UserAccount.new
     @user_account.role = @role
   end
 
   # POST /resource
   def create
-    #byebug
+    
     user = UserAccount.new(email: params[:user_account][:email], name: params[:user_account][:name],
                            id_card: params[:user_account][:id_card], campus_id: Campus.first.id,
                            role: params[:user_account][:role])
@@ -32,7 +32,7 @@ class UserAccounts::RegistrationsController < Devise::RegistrationsController
 
   # PUT /resource
   def update
-    #byebug
+    
     super
   end
 
